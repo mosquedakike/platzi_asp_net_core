b722fd864489cd01dee1eb6dd9e9723b58175d63
mosquedakike <enrique_inf@hotmail.com>
2020-04-11 20:33:57 -0500

    Aplicando tabla en MultiAsignatura

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -38,7 +38,10 @@ namespace platzi_asp_net_core.Controllers
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}", 
+                                                   UniqueId = Guid.NewGuid().ToString() 
+                               };
+                               
 
             return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
         }
