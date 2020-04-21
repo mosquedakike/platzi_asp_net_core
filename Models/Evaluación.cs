2f1977f28d50044fada3fb3ea1d7b11469d1cb96
Enrique <enrique_inf@hotmail.com>
2020-04-20 23:10:45 -0500

    Implementando cambios en el modelo

--- Models/Evaluación.cs
+++ Models/Evaluación.cs
@@ -2,10 +2,12 @@ using System;
 
 namespace platzi_asp_net_core.Models
 {
-    public class Evaluación:ObjetoEscuelaBase
+    public class Evaluación : ObjetoEscuelaBase
     {
         public Alumno Alumno { get; set; }
-        public Asignatura Asignatura  { get; set; }
+        public string AlumnoId { get; set; }
+        public Asignatura Asignatura { get; set; }
+        public string AsignaturaId { get; set; }
 
         public float Nota { get; set; }
 
